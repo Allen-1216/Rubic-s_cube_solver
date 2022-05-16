907f7712141f5d35776ecba35b484d6e634788a8
Allen-1216 <allen891216@gmail.com>
2022-05-16 20:04:13 +0800

    0.1.6

--- main.py
+++ main.py
@@ -8,12 +8,8 @@ from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
 import simulation_cube
 #import kociemba
-#import numpy as np
-# This program requires LEGO EV3 MicroPython v2.0 or higher.
-# Click "Open user guide" on the EV3 extension tab for more information.
 
 
-# Initialize the EV3 Brick.
 ev3 = EV3Brick()
 
 motorA = Motor(Port.A) #A馬達 正值手臂抬起
@@ -32,6 +28,7 @@ def scan_color(): #讀取單顆顏色
     global x, y
     RGB = [0, 0, 0]
     RGB = ColorSensorS4.rgb()
+    #cube3d[x][y] = ColorSensorS4.rgb() #RGB測試用
     strcolor = ColorSensorS4.color()
     if strcolor == Color.BLUE:
         cube3d[x][y] = 'U'
@@ -43,9 +40,9 @@ def scan_color(): #讀取單顆顏色
         cube3d[x][y] = 'B'
     elif strcolor == Color.GREEN:
         cube3d[x][y] = 'D'
-    #cube3d[x][y] = ColorSensorS4.color() #lego掃不到橘色，會把橘當成紅色，所以先把可辨認的顏色排除，留下紅色，再用RGB光反射作區別
+    #lego掃不到橘色，會把橘當成紅色，所以先把可辨認的顏色排除，留下紅色，再用RGB光反射作區別
     if cube3d[x][y] == 'R':
-        if RGB[0] > 50:
+        if RGB[0] > 58: #隨環境不同數字需修正
             cube3d[x][y] = 'L'
         else:
             cube3d[x][y] = 'R'
@@ -56,20 +53,20 @@ def scan_color(): #讀取單顆顏色
 
 def scan_plane(): #讀取一面的各個顏色
     motorC.run_until_stalled(300,then=Stop.HOLD, duty_limit=None) #後退到最底 1
-    motorC.run_angle(300, -235*3, then=Stop.HOLD, wait=True) #往前到中間 2
+    motorC.run_angle(300, -230*3, then=Stop.HOLD, wait=True) #往前到中間 2
     scan_color()
-    motorB.run_angle(300, -45*3, then=Stop.HOLD, wait=True)#逆時針轉45度
+    motorB.run_angle(300, -45*3, then=Stop.HOLD, wait=True) #底盤逆時針轉45度
     motorC.run_angle(300, 60*3, then=Stop.HOLD, wait=True) #後退掃描角落的 3
     scan_color()
     for r in range (0, 3):
         motorB.run_angle(300, -45*3, then=Stop.HOLD, wait=True)
-        motorC.run_angle(300, -30*3, then=Stop.HOLD, wait=True) #前進掃描邊邊中間4、6、8
+        motorC.run_angle(300, -35*3, then=Stop.HOLD, wait=True) #前進掃描邊邊中間4、6、8
         scan_color()
         motorB.run_angle(300, -45*3, then=Stop.HOLD, wait=True)
-        motorC.run_angle(300, 30*3, then=Stop.HOLD, wait=True) #後退掃描角落5、7、9
+        motorC.run_angle(300, 35*3, then=Stop.HOLD, wait=True) #後退掃描角落5、7、9
         scan_color()
     motorB.run_angle(300, -45*3, then=Stop.HOLD, wait=True)
-    motorC.run_angle(300, -30*3, then=Stop.HOLD, wait=True) #前進掃描邊邊中間 10
+    motorC.run_angle(300, -35*3, then=Stop.HOLD, wait=True) #前進掃描邊邊的中間 10
     scan_color()
     motorC.run_until_stalled(300,then=Stop.HOLD, duty_limit=None) #後退到最底 11
 
@@ -78,13 +75,13 @@ def roll_cube(): #馬達A手臂翻轉方塊
     motorA.run_angle(250, 110)
     motorA.run_angle(250, -110)
     motorA.run_until_stalled(-300,then=Stop.HOLD, duty_limit=None) #手臂抬起
-    
+
 
 #motorC.run_until_stalled(300,then=Stop.HOLD, duty_limit=None) #測試用 顏色感應器後退到最底
 
 #掃描6面
 scan_plane() #掃U
-motorB.run_angle(300, 90*3, then=Stop.HOLD, wait=True)#底盤順時針轉90度
+motorB.run_angle(300, 180*3, then=Stop.HOLD, wait=True)#底盤順時針轉180度
 roll_cube()
 scan_plane() #掃L
 motorB.run_angle(300, 90*3, then=Stop.HOLD, wait=True)#底盤順時針轉90度
@@ -107,31 +104,32 @@ for i in range(2):
     motorA.run_angle(250, -110)
     wait(50)
 motorA.run_until_stalled(-300,then=Stop.HOLD, duty_limit=None) #手臂抬起
+motorB.run_angle(300, -90*3, then=Stop.HOLD, wait=True)#底盤逆時針轉90度
 
 
 simulation_cube3d = cube3d.copy() #複製一個新陣列 模擬方塊當下的狀態
 kociemba_cube3d = cube3d.copy() #複製一個新陣列 儲存符合kociemba排列順序
 simulation_cube = simulation_cube.Cube(simulation_cube3d)#建立物件 模擬旋轉方塊
 #檢查用(中文方便辨識)
-#cubexxx = [['藍', '藍', '藍', '藍', '藍', '藍', '藍', '藍', '藍'], ['橘', '橘', '橘', '橘', '橘', '橘', '橘', '橘', '橘'], ['黃', '黃', '黃', '黃', '黃', '黃', '黃', '黃', '黃'], ['紅', '紅', '紅', '紅', '紅', '紅', '紅', '紅', '紅'], ['白', '白', '白', '白', '白', '白', '白', '白', '白'], ['綠', '綠', '綠', '綠', '綠', '綠', '綠', '綠', '綠']]
+#cube3d = [['藍', '藍', '藍', '藍', '藍', '藍', '藍', '藍', '藍'], ['橘', '橘', '橘', '橘', '橘', '橘', '橘', '橘', '橘'], ['黃', '黃', '黃', '黃', '黃', '黃', '黃', '黃', '黃'], ['紅', '紅', '紅', '紅', '紅', '紅', '紅', '紅', '紅'], ['白', '白', '白', '白', '白', '白', '白', '白', '白'], ['綠', '綠', '綠', '綠', '綠', '綠', '綠', '綠', '綠']]
 
 #符合隨機方塊演算法排序的陣列
 simulation_cube3d = \
-[str(cube3d[0][7]) + str(cube3d[0][8]) + str(cube3d[0][1]) + str(cube3d[0][6]) + str(cube3d[0][0]) + str(cube3d[0][2]) + str(cube3d[0][5]) + str(cube3d[0][4]) + str(cube3d[0][3])]\
+ [str(cube3d[0][5]) + str(cube3d[0][6]) + str(cube3d[0][7]) + str(cube3d[0][4]) + str(cube3d[0][0]) + str(cube3d[0][8]) + str(cube3d[0][3]) + str(cube3d[0][2]) + str(cube3d[0][1])]\
++[str(cube3d[1][3]) + str(cube3d[1][4]) + str(cube3d[1][5]) + str(cube3d[1][2]) + str(cube3d[1][0]) + str(cube3d[1][6]) + str(cube3d[1][1]) + str(cube3d[1][8]) + str(cube3d[1][7])]\
 +[str(cube3d[2][5]) + str(cube3d[2][6]) + str(cube3d[2][7]) + str(cube3d[2][4]) + str(cube3d[2][0]) + str(cube3d[2][8]) + str(cube3d[2][3]) + str(cube3d[2][2]) + str(cube3d[2][1])]\
 +[str(cube3d[3][5]) + str(cube3d[3][6]) + str(cube3d[3][7]) + str(cube3d[3][4]) + str(cube3d[3][0]) + str(cube3d[3][8]) + str(cube3d[3][3]) + str(cube3d[3][2]) + str(cube3d[3][1])]\
 +[str(cube3d[4][5]) + str(cube3d[4][6]) + str(cube3d[4][7]) + str(cube3d[4][4]) + str(cube3d[4][0]) + str(cube3d[4][8]) + str(cube3d[4][3]) + str(cube3d[4][2]) + str(cube3d[4][1])]\
-+[str(cube3d[1][3]) + str(cube3d[1][4]) + str(cube3d[1][5]) + str(cube3d[1][2]) + str(cube3d[1][0]) + str(cube3d[1][6]) + str(cube3d[1][1]) + str(cube3d[1][8]) + str(cube3d[1][7])]\
 +[str(cube3d[5][7]) + str(cube3d[5][8]) + str(cube3d[5][1]) + str(cube3d[5][6]) + str(cube3d[5][0]) + str(cube3d[5][2]) + str(cube3d[5][5]) + str(cube3d[5][4]) + str(cube3d[5][3])]
 
 #符合kociemba演算法排序的陣列
 kociemba_cube3d = \
-str(cube3d[0][7]) + str(cube3d[0][8]) + str(cube3d[0][1]) + str(cube3d[0][6]) + str(cube3d[0][0]) + str(cube3d[0][2]) + str(cube3d[0][5]) + str(cube3d[0][4]) + str(cube3d[0][3])\
-+str(cube3d[4][5]) + str(cube3d[4][6]) + str(cube3d[4][7]) + str(cube3d[4][4]) + str(cube3d[4][0]) + str(cube3d[4][8]) + str(cube3d[4][3]) + str(cube3d[4][2]) + str(cube3d[4][1])\
+ str(cube3d[0][5]) + str(cube3d[0][6]) + str(cube3d[0][7]) + str(cube3d[0][4]) + str(cube3d[0][0]) + str(cube3d[0][8]) + str(cube3d[0][3]) + str(cube3d[0][2]) + str(cube3d[0][1])\
 +str(cube3d[3][5]) + str(cube3d[3][6]) + str(cube3d[3][7]) + str(cube3d[3][4]) + str(cube3d[3][0]) + str(cube3d[3][8]) + str(cube3d[3][3]) + str(cube3d[3][2]) + str(cube3d[3][1])\
-+str(cube3d[5][7]) + str(cube3d[5][8]) + str(cube3d[5][1]) + str(cube3d[5][6]) + str(cube3d[5][0]) + str(cube3d[5][2]) + str(cube3d[5][5]) + str(cube3d[5][4]) + str(cube3d[5][3])\
 +str(cube3d[2][5]) + str(cube3d[2][6]) + str(cube3d[2][7]) + str(cube3d[2][4]) + str(cube3d[2][0]) + str(cube3d[2][8]) + str(cube3d[2][3]) + str(cube3d[2][2]) + str(cube3d[2][1])\
-+str(cube3d[1][3]) + str(cube3d[1][4]) + str(cube3d[1][5]) + str(cube3d[1][2]) + str(cube3d[1][0]) + str(cube3d[1][6]) + str(cube3d[1][1]) + str(cube3d[1][8]) + str(cube3d[1][7])
++str(cube3d[5][7]) + str(cube3d[5][8]) + str(cube3d[5][1]) + str(cube3d[5][6]) + str(cube3d[5][0]) + str(cube3d[5][2]) + str(cube3d[5][5]) + str(cube3d[5][4]) + str(cube3d[5][3])\
++str(cube3d[1][3]) + str(cube3d[1][4]) + str(cube3d[1][5]) + str(cube3d[1][2]) + str(cube3d[1][0]) + str(cube3d[1][6]) + str(cube3d[1][1]) + str(cube3d[1][8]) + str(cube3d[1][7])\
++str(cube3d[4][5]) + str(cube3d[4][6]) + str(cube3d[4][7]) + str(cube3d[4][4]) + str(cube3d[4][0]) + str(cube3d[4][8]) + str(cube3d[4][3]) + str(cube3d[4][2]) + str(cube3d[4][1])
 
 
 
@@ -143,8 +141,10 @@ for i in range(6):
 print()
 
 print(kociemba_cube3d)
+print()
 
-#print("解法: " + kociemba.solve(kociemba_cube3d) + "\n")
+
+#print("解法: " + kociemba.solve("BBURUDBFUFFFRRFUUFLULUFUDLRRDBBDBDBLUDDFLLRRBRLLLBRDDF")) debug中
 
 #wait(5000)
 #ev3.screen.clear()
--- simulation_cube.py
+++ simulation_cube.py
@@ -3,18 +3,57 @@ class Cube:
     def __init__(self,cube):
         self.cube = cube
 
-    def faceMove(self, x):
+    def faceMove(self, x): #面順轉
         self.cube[x][0], self.cube[x][6], self.cube[x][8], self.cube[x][2] = self.cube[x][6], self.cube[x][8], self.cube[x][2], self.cube[x][0]
         self.cube[x][1], self.cube[x][7], self.cube[x][5], self.cube[x][3] = self.cube[x][3], self.cube[x][5], self.cube[x][1], self.cube[x][7]
         return
 
-    def faceMovePrime(self, x):
+    def faceMove_reverse(self, x): #面逆轉
         self.cube[x][0], self.cube[x][2], self.cube[x][8], self.cube[x][6] = self.cube[x][2], self.cube[x][8], self.cube[x][6], self.cube[x][0]
         self.cube[x][1], self.cube[x][3], self.cube[x][5], self.cube[x][7] = self.cube[x][5], self.cube[x][1], self.cube[x][7], self.cube[x][3]
         return
 
-    def swap(self, x1, x2, x3, x4, y1, y2, y3, y4):
+    def swap(self, x1, x2, x3, x4, y1, y2, y3, y4): #4顆顏色交換
         self.cube[x1][y1], self.cube[x2][y2], self.cube[x3][y3], self.cube[x4][y4] = self.cube[x2][y2], self.cube[x3][y3], self.cube[x4][y4], self.cube[x1][y1]
+    
+    def roll(self): #拉推模擬
+        self.faceMove_reverse(2)
+        self.faceMove_reverse(4)
+        self.swap(1, 0, 3, 5, 6, 0, 2, 8)
+        self.swap(1, 0, 3, 5, 7, 3, 1, 5)
+        self.swap(1, 0, 3, 5, 8, 6, 0, 2)
+        self.swap(1, 0, 3, 5, 3, 1, 5, 7)
+        self.swap(1, 0, 3, 5, 4, 4, 4, 4)
+        self.swap(1, 0, 3, 5, 5, 7, 3, 1)
+        self.swap(1, 0, 3, 5, 0, 2, 8, 6)
+        self.swap(1, 0, 3, 5, 1, 5, 7, 3)
+        self.swap(1, 0, 3, 5, 2, 8, 6, 0)
+
+    def baseMove(self): #底盤順轉模擬 90度為一單位
+        self.faceMove(0)
+        self.faceMove(5)
+        self.swap(1, 2, 3, 4, 0, 0, 0, 0)
+        self.swap(1, 2, 3, 4, 1, 1, 1, 1)
+        self.swap(1, 2, 3, 4, 2, 2, 2, 2)
+        self.swap(1, 2, 3, 4, 3, 3, 3, 3)
+        self.swap(1, 2, 3, 4, 4, 4, 4, 4)
+        self.swap(1, 2, 3, 4, 5, 5, 5, 5)
+        self.swap(1, 2, 3, 4, 6, 6, 6, 6)
+        self.swap(1, 2, 3, 4, 7, 7, 7, 7)
+        self.swap(1, 2, 3, 4, 8, 8, 8, 8)
+
+    def baseMove_reverse(self): #底盤逆轉模擬 90度為一單位
+        self.faceMove_reverse(0)
+        self.faceMove_reverse(5)
+        self.swap(4, 3, 2, 1, 0, 0, 0, 0)
+        self.swap(4, 3, 2, 1, 1, 1, 1, 1)
+        self.swap(4, 3, 2, 1, 2, 2, 2, 2)
+        self.swap(4, 3, 2, 1, 3, 3, 3, 3)
+        self.swap(4, 3, 2, 1, 4, 4, 4, 4)
+        self.swap(4, 3, 2, 1, 5, 5, 5, 5)
+        self.swap(4, 3, 2, 1, 6, 6, 6, 6)
+        self.swap(4, 3, 2, 1, 7, 7, 7, 7)
+        self.swap(4, 3, 2, 1, 8, 8, 8, 8)
 
 def move(cube, m, x):
     # Need to do 3 swaps based on the move
@@ -24,7 +63,7 @@ def move(cube, m, x):
         cube.swap(1, 2, 3, 4, 1, 1, 1, 1)
         cube.swap(1, 2, 3, 4, 2, 2, 2, 2)
     elif (m == "U'"):
-        cube.faceMovePrime(x)
+        cube.faceMove_reverse(x)
         cube.swap(1, 4, 3, 2, 0, 0, 0, 0)
         cube.swap(1, 4, 3, 2, 1, 1, 1, 1)
         cube.swap(1, 4, 3, 2, 2, 2, 2, 2)
@@ -37,7 +76,7 @@ def move(cube, m, x):
         cube.swap(4, 3, 2, 1, 7, 7, 7, 7)
         cube.swap(4, 3, 2, 1, 8, 8, 8, 8)
     elif (m == "D'"):
-        cube.faceMovePrime(x)
+        cube.faceMove_reverse(x)
         cube.swap(1, 2, 3, 4, 6, 6, 6, 6)
         cube.swap(1, 2, 3, 4, 7, 7, 7, 7)
         cube.swap(1, 2, 3, 4, 8, 8, 8, 8)
@@ -50,7 +89,7 @@ def move(cube, m, x):
         cube.swap(0, 2, 5, 4, 5, 5, 5, 3)
         cube.swap(0, 2, 5, 4, 8, 8, 8, 0)
     elif (m == "R'"):
-        cube.faceMovePrime(x)
+        cube.faceMove_reverse(x)
         cube.swap(5, 2, 0, 4, 2, 2, 2, 6)
         cube.swap(5, 2, 0, 4, 5, 5, 5, 3)
         cube.swap(5, 2, 0, 4, 8, 8, 8, 0)
@@ -63,7 +102,7 @@ def move(cube, m, x):
         cube.swap(0, 4, 5, 2, 3, 5, 3, 3)
         cube.swap(0, 4, 5, 2, 0, 2, 0, 0)
     elif (m == "L'"):
-        cube.faceMovePrime(x)
+        cube.faceMove_reverse(x)
         cube.swap(5, 4, 0, 2, 6, 8, 6, 6)
         cube.swap(5, 4, 0, 2, 3, 5, 3, 3)
         cube.swap(5, 4, 0, 2, 0, 2, 0, 0)
@@ -76,7 +115,7 @@ def move(cube, m, x):
         cube.swap(0, 1, 5, 3, 7, 5, 1, 3)
         cube.swap(0, 1, 5, 3, 6, 8, 2, 0)
     elif (m == "F'"):
-        cube.faceMovePrime(x)
+        cube.faceMove_reverse(x)
         cube.swap(0, 3, 5, 1, 8, 6, 0, 2)
         cube.swap(0, 3, 5, 1, 7, 3, 1, 5)
         cube.swap(0, 3, 5, 1, 6, 0, 2, 8)
@@ -89,7 +128,7 @@ def move(cube, m, x):
         cube.swap(0, 3, 5, 1, 1, 5, 7, 3)
         cube.swap(0, 3, 5, 1, 2, 8, 6, 0)
     elif (m == "B'"):
-        cube.faceMovePrime(x)
+        cube.faceMove_reverse(x)
         cube.swap(0, 1, 5, 3, 0, 6, 8, 2)
         cube.swap(0, 1, 5, 3, 1, 3, 7, 5)
         cube.swap(0, 1, 5, 3, 2, 0, 6, 8)
